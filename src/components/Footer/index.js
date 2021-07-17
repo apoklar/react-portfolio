e80c0363b0dd4b01136ce7555f5ada5d1076c07c
andrp <a.poklar@yahoo.com>
2021-07-16 22:47:07 -0500

    finish footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -2,13 +2,18 @@ import React from 'react';
 
 function Footer() {
     return (
-        <div>  
+        <div className="page-container">
+          <div className="content-wrap">  
             <link href="https://fonts.googleapis.com/css2?family=Comfortaa:wght@300&display=swap" rel="stylesheet"></link>
+            <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"></link>
+            <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"></link>
             <footer className="footer">
-            <p className="footer-text">This is some content in sticky footer</p>
-            </footer>  
+            <p className="footer-text"></p>
+            <a href="https://www.github.com/apoklar" className="github"><i class="fa fa-github fa-4x"></i></a>
+            <a href="https://www.linkedin.com/in/andrea-poklar-b0a457203/" className="linkedin"> <i class="fa fa-linkedin-square fa-4x"></i></a>
+            </footer>
+          </div>  
         </div>
     )
 }
-
 export default Footer;
