f6f2dd3aa3c00b2c6a6d1d30b44275ecc4d5a4d4
andrp <a.poklar@yahoo.com>
2021-07-16 22:57:23 -0500

    about page fixed

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -2,11 +2,13 @@ import React from 'react'
 
 function About() {
     return (
-        <div>
+        <div className="about-page">
             <link href="https://fonts.googleapis.com/css2?family=Comfortaa:wght@300&display=swap" rel="stylesheet"></link>
             <h1 data-testid="h1tag" className="title">About Me</h1>
 
-            <h3 className="about">Full stack web developer with a background in some mathematics and engineering from the University of Houston and also received an Associates of Science. Recent graduate of the coding bootcamp from the University of Texas at Austin where I earned a Certificate in Full Stack Web Development. Some skill sets include Javascript, MongoDB, and React.js. I enjoy working with people and learning new things. Strengths in creativity, teamwork, and being teachable. My biggest goal with each project I do is to use my creativity and knowledge of web developing to create apps and websites that look and function well. </h3>
+            <h3 className="about">Full stack web developer with a background in some mathematics and engineering from the University of Houston and also received an Associates of Science. Recent graduate of the coding bootcamp from the University of Texas at Austin where I earned a Certificate in Full Stack Web Development.</h3>
+            <b></b>
+            <h3 className="about">Some skill sets include Javascript, MongoDB, and React.js. I enjoy working with people and learning new things. Strengths in creativity, teamwork, and being teachable. My biggest goal with each project I do is to use my creativity and knowledge of web developing to create apps and websites that look and function well.</h3>
         </div>
     )
 }
