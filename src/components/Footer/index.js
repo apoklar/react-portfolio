b3ccaa0e46332a7fc7108f21c54c186eb9074fd4
andrp <a.poklar@yahoo.com>
2021-07-16 22:02:07 -0500

    footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -2,11 +2,11 @@ import React from 'react';
 
 function Footer() {
     return (
-        <div classname="footer">
+        <div>  
             <link href="https://fonts.googleapis.com/css2?family=Comfortaa:wght@300&display=swap" rel="stylesheet"></link>
-            <footer>
+            <footer className="footer">
             <p className="footer-text">This is some content in sticky footer</p>
-            </footer>
+            </footer>  
         </div>
     )
 }
