28bf10cbc2677d813157489c90bd697b8b8f7c43
andrp <a.poklar@yahoo.com>
2021-11-14 16:03:03 -0600

    fixed project page

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -9,6 +9,8 @@ function About() {
             <h3 className="about">Full stack web developer with a background in some mathematics and engineering from the University of Houston and also received an Associates of Science. Recent graduate of the coding bootcamp from the University of Texas at Austin where I earned a Certificate in Full Stack Web Development.</h3>
             <b></b>
             <h3 className="about">Some skill sets include Javascript, MongoDB, and React.js. I enjoy working with people and learning new things. Strengths in creativity, teamwork, and being teachable. My biggest goal with each project I do is to use my creativity and knowledge of web developing to create apps and websites that look and function well.</h3>
+            <b></b>
+            <h3 className="about">In my free time, I prefer to stay busy. I do creative things such as painting, singing, and crocheting. I also like to go running.</h3>
         </div>
     )
 }
--- src/components/Project/index.js
+++ src/components/Project/index.js
@@ -1,9 +1,22 @@
-import React from 'react'
+import React from 'react';
+import Photo from "../images/catch-pokemon.jpg";
 
 function Project() {
     return (
         <div>
+        <link href="https://fonts.googleapis.com/css2?family=Comfortaa:wght@300&display=swap" rel="stylesheet"></link>
             <h1 data-testid="h1tag" className="title">Projects</h1>
+            
+            <div>
+            <a href="https://bc-project-2.herokuapp.com/" className="img">
+            <img src={Photo} alt="Pokemon Team Builder" width="500px" height="250px"></img></a>
+            </div>
+            <b></b>
+            <div>
+            <a href="https://mikeyrod22.github.io/what-would-kanye-do/" className="img">
+            <img src="public/catch-pokemon.jpg" alt="What Would Kanye Do?" width="500px" height="250px"></img></a>    
+            </div>
+
         </div>
     )
 }
