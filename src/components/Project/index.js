6e9e1a3895c5be44f0df371fe51bcbeb33de7b64
andrp <a.poklar@yahoo.com>
2021-11-17 23:12:17 -0600

    cleaned portfolio page

--- src/components/Project/index.js
+++ src/components/Project/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import Photo from "../images/catch-pokemon.jpg";
+import pokemon from './images/catch-pokemon.jpg';
+import movie from './images/main-page.png';
 
 function Project() {
     return (
@@ -7,16 +8,21 @@ function Project() {
         <link href="https://fonts.googleapis.com/css2?family=Comfortaa:wght@300&display=swap" rel="stylesheet"></link>
             <h1 data-testid="h1tag" className="title">Projects</h1>
             
-            <div>
+            <div className="pokemon-img">
             <a href="https://bc-project-2.herokuapp.com/" className="img">
-            <img src={Photo} alt="Pokemon Team Builder" width="500px" height="250px"></img></a>
+            <img src={pokemon} alt="Pokemon Team Builder" width="500px" height="250px"></img></a>
+                <div className="overlay">
+                    <div className="text">Pokemon Team Builder</div>
+                </div>  
             </div>
-            <b></b>
+            {' '}
             <div>
-            <a href="https://mikeyrod22.github.io/what-would-kanye-do/" className="img">
-            <img src="public/catch-pokemon.jpg" alt="What Would Kanye Do?" width="500px" height="250px"></img></a>    
+            <a href="http://aqueous-reef-39758.herokuapp.com/" className="img">
+            <img src={movie} alt="Movie Search" width="500px" height="350px"></img></a>
             </div>
-
+                <div className="overlay">
+                    <div className="text">Movie Search</div>
+                </div>  
         </div>
     )
 }
